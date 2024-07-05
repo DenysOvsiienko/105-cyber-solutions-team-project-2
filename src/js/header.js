5a48f824a90328b08354442d7f4611b3d93182d9
Denys <denis.itsme.1@gmail.com>
2024-07-05 15:22:25 +0300

    header.js & reset.css update

--- src/js/header.js
+++ src/js/header.js
@@ -4,11 +4,12 @@ navMenuBtnElem.addEventListener('click', event => {
   event.stopPropagation();
   navMenuListElem.classList.toggle('visually-hidden');
   if (navMenuListElem.classList.contains('visually-hidden')) {
-    navMenuListElem.style.opacity = '';
+    navMenuListElem.removeAttribute('style');
   } else {
     navMenuListElem.style.opacity = '1';
   }
 });
 document.addEventListener('click', () => {
   navMenuListElem.classList.add('visually-hidden');
+  navMenuListElem.removeAttribute('style');
 });
