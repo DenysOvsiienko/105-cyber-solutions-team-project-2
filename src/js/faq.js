b011c475d9440cecae53e373dac75f6f12203546
Denys <denis.itsme.1@gmail.com>
2024-07-07 16:34:35 +0300

    faq js

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,11 +2,3 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 new Accordion('.accordion-container');
-
-new Accordion('.container-second', {
-    duration: 400,
-    showMultiple: true,
-    onOpen: function(currentElement) {
-      console.log(currentElement);
-    }
-  });
