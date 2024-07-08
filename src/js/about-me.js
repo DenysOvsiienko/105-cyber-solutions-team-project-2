01088756815495639ec3778bafe8194373bad13f
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-07-08 15:12:00 +0300

    add about-me-js

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,43 +1,19 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+import Accordion from 'accordion-js';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const accordionItems = document.querySelectorAll('.accordion-item-about-me');
-
-  accordionItems.forEach((item, index) => {
-    const trigger = item.querySelector('.aboutme-accordion-trigger');
-    const panel = item.querySelector('.accordion-panel-about-me');
-
-    if (index !== 0) {
-      item.classList.remove('is-active');
-      panel.style.display = 'none';
-    }
-
-    trigger.addEventListener('click', function () {
-      const isOpen = item.classList.contains('is-active');
-
-      // accordionItems.forEach(i => {
-      //   i.classList.remove('is-active');
-      //   i.querySelector('.aboutme-accordion-panel').style.display = 'none';
-      // });
-
-      // if (!isOpen) {
-      //   item.classList.add('is-active');
-      //   panel.style.display = 'flex';
-      // }
-
-      if (isOpen) {
-        item.classList.remove('is-active');
-        panel.style.display = 'none';
-      } else {
-        item.classList.add('is-active');
-        panel.style.display = 'flex';
-      }
-    });
-  });
+import 'accordion-js/dist/accordion.min.css';
+const aboutMeAccordion = new Accordion('.accordion-container-about-me', {
+  duration: 600,
+  showMultiple: true,
+  openOnInit: [0],
+  elementClass: 'accordion-item-about-me',
+  triggerClass: 'accordion-trigger-about-me',
+  panelClass: 'accordion-panel-about-me',
 });
 
-// Swiper
+// // Swiper
+
 document.addEventListener('DOMContentLoaded', function () {
   const slides = document.querySelectorAll('.about-skills-circle');
   const swiperWrapper = document.querySelector('.swiper-wrapper');
