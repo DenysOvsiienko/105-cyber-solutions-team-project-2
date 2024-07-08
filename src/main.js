7506df40eace8dcc88acd2f70bc50fa895c4e288
Denys <denis.itsme.1@gmail.com>
2024-07-08 15:08:22 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import './js/header';
 import './js/covers';
 import './js/faq';
 import './js/projects';
-// import './js/work-together';
+import './js/work-together';
 import './js/reviews';
 import './js/reviews-render-functions';
