8bcf7986ddea7a409e5a9bb6a2d3d288659b5edf
Denys <denis.itsme.1@gmail.com>
2024-07-08 09:59:54 +0300

    js libraries revision for reviews & faq sections

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -7,17 +7,16 @@ import 'swiper/css/bundle';
 const urlapi = 'https://portfolio-js.b.goit.study/api/reviews';
 
 const reviewlist = document.querySelector('.reviews-list');
-const prevbtnEl = document.querySelector('.js-btn-prev');
+const prevbtnEl = document.querySelector('.reviews-js-btn-prev');
 4;
 
 // console.dir(prevbtnEl);
-const nextbtnEl = document.querySelector('.js-btn-next');
+const nextbtnEl = document.querySelector('.reviews-js-btn-next');
 // console.dir(nextbtnEl);
-const prevsvgbtn = document.querySelectorAll('.js-icon-prev');
+const prevsvgbtn = document.querySelectorAll('.reviews-js-icon-prev');
 // console.dir(prevsvgbtn);
-const nextsvgbtn = document.querySelectorAll('.js-icon-next');
+const nextsvgbtn = document.querySelectorAll('.reviews-js-icon-next');
 // console.dir(nextsvgbtn);
-
 
 const fetchReviews = async url => {
   try {
@@ -37,13 +36,13 @@ fetchReviews(urlapi).then(reviews => {
   // console.log('Отримані відгуки:', reviews);
   reviewlist.innerHTML = createReviewsList(reviews);
 
-  const reviewsswiper = new Swiper('.swiper-reviews', {
+  const reviewsSwiper = new Swiper('.swiper-reviews', {
     modules: Navigation,
     direction: 'horizontal',
     slidesPerView: 1,
     slidesPerGroup: 1,
     spaceBetween: 18,
-
+    wrapperClass: 'reviews-list',
     breakpoints: {
       768: {
         slidesPerView: 2,
@@ -57,9 +56,12 @@ fetchReviews(urlapi).then(reviews => {
       },
     },
     navigation: {
-      nextEl: '.js-btn-next',
-      prevEl: '.js-btn-prev',
+      nextEl: '.reviews-js-btn-next',
+      prevEl: '.reviews-js-btn-prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
     },
   });
 });
-
